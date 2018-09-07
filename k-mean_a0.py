544e004628637ecfdb479bde9724d3a2608f1147
Reza Asad <rayza.asad@gmail.com>
2018-09-07 10:02:05 -0700

    creating the repo

---
+++ k-mean_a0.py
@@ -0,0 +1,71 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def k_mean(samples, num_clusters, stop_epsion=1e-2, max_iter=100):
+    """
+    K-Mean Cluster Implementation
+    :param samples: samples with dimension of (num_points, 2)
+    :param num_clusters: number of clusters
+    :param stop_epsion: stop condition
+    :param max_iter: allowed max iteration
+    :return cluster_loc: center of each cluster
+    :return sample_cluster_index: cluster indices for each sample points 
+    """
+
+    # Cluster indices
+    sample_cluster_index = np.zeros(samples.shape[0], dtype=np.int)
+
+    # Distance cache, dim: (num_clusters, num_samples)
+    sample_cluster_distance = np.zeros((num_clusters, samples.shape[0]), dtype=np.float32) 
+
+    # Step 1: Random choose initial points as cluster center
+    random_indices = np.arange(0, samples.shape[0], dtype=np.int)
+    np.random.shuffle(random_indices)
+    cluster_loc = samples[random_indices[:num_clusters], :]
+    old_distance_var = -10000
+
+    # Step 2:Iteration
+    for itr in range(0, max_iter):
+
+        # Instruction: Fill the following blanks with your implementation, you should finish the implementation with less than 25 lines of code. 
+        
+        # Compute the distance towards the cluster center, you can use 'np.linalg.norm' to compute L2 distance
+        # [TODO] Fill your code here
+
+        # For each sample point, set the cluster center with minimal distance, tip: use np.argmin to find the index that has minimal value 
+        # [TODO] Fill your code here
+
+        # Re-compute the distance by average the cluster sampled points, and update the 'cluster_loc'
+        # [TODO] Fill your code here
+
+        # Compute total avg. distance variance
+        # [TODO] Fill your code here
+        avg_distance_var = 0 
+
+        # Check if the avg. distance variance has converged
+        if np.abs(avg_distance_var - old_distance_var) < stop_epsion:
+            break
+
+        print("Itr %d, avg. distance variance: %f" % (itr, avg_distance_var))
+        old_distance_var = avg_distance_var
+
+    return cluster_loc, sample_cluster_index 
+
+
+# Load the sample points
+points = np.load('k-mean_samples.npy')
+
+# Do K-Mean cluster
+cluster_loc, cluster_indices = k_mean(points, num_clusters=6)
+
+# Draw the clusters
+colors = ['b', 'g', 'r', 'c', 'm', 'y', 'w']
+for cluster_idx in range(0, cluster_loc.shape[0]):
+    sub_sample_set = points[cluster_indices == cluster_idx]
+    plt.scatter(sub_sample_set[:, 0], sub_sample_set[:, 1], c=colors[cluster_idx], label='group %d' % cluster_idx)
+
+plt.scatter(cluster_loc[:, 0], cluster_loc[:, 1], c='k', label='center')
+plt.legend()
+plt.grid(True)
+plt.title("K-Mean Cluster (%d centers)" % cluster_loc.shape[0])
+plt.show()
